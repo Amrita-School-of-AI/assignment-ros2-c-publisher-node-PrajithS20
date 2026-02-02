e0828a38136d5357b6dfd8e1bf4e0b43c62203f0
PrajithS20 <prajithprani@gmail.com>
2026-02-02 23:17:03 +0530

    done

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -7,7 +7,6 @@
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
-
 /*
  * TODO: Create a Class named 'PublisherNode' that inherits from rclcpp::Node.
  * Requirements:
@@ -28,12 +27,24 @@ public:
         : Node("publisher_node"), count_(0)
     {
         // TODO: Create the publisher here
-
+	publisher_ = this->create_publisher<std_msgs::msg::String>("/counter",10);
         // TODO: Initialize the timer here
+	timer_ = this->create_wall_timer(
+	  500ms, std::bind(&PublisherNode::timer_callback, this));
     }
 
 private:
     // TODO: Define the timer_callback function here
+    void timer_callback()
+    {
+    auto message =std_msgs::msg::String();
+
+    message.data="Count: "+ std::to_string(count_++);
+
+    RCLCPP_INFO(this->get_logger(),"Publishing: '%s'", message.data.c_str());
+
+    publisher_->publish(message);
+    }
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
